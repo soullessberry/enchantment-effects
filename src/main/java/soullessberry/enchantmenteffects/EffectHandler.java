8e8c0de0ef3428536aad01f79ff9e819d6e2f011
soullessberry <skullkidbryan@gmail.com>
2026-05-02 13:33:58 -0700

    Implement fire aspect

--- src/main/java/soullessberry/enchantmenteffects/EffectHandler.java
+++ src/main/java/soullessberry/enchantmenteffects/EffectHandler.java
@@ -5,8 +5,10 @@ import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.util.Mth;
@@ -32,6 +34,7 @@ public class EffectHandler {
 
     public static void applyEffects(Player player, Entity target) {
         attemptBaneOfArthropods(player, target);
+        attemptFireAspect(player, target);
         attemptSharpness(player, target);
         attemptSmite(player, target);
     }
@@ -40,6 +43,13 @@ public class EffectHandler {
         int baneLevel = getEnchantmentLevel(player.getWeaponItem(), Enchantments.BANE_OF_ARTHROPODS);
         if (target.is(EntityTypeTags.ARTHROPOD) && baneLevel > 0) {
             applyBaneEffect(target, baneLevel);
+        }
+    }
+
+    private static void attemptFireAspect(Player player, Entity target) {
+        int fireLevel = getEnchantmentLevel(player.getWeaponItem(), Enchantments.FIRE_ASPECT);
+        if (!target.fireImmune() && fireLevel > 0) {
+            applyFireEffect(target, fireLevel);
         }
     }
 
@@ -78,6 +88,17 @@ public class EffectHandler {
         float offset = target.getDimensions(target.getPose()).height() / 2 + 0.25f;
         float scale = 0.15f * baneLevel + 0.25f;
         spawnBaneParticle(target.level(), target.position().add(0, offset, 0), scale);
+    }
+
+    private static void applyFireEffect(Entity target, int fireLevel) {
+        playSoundEffect(
+                SoundEvents.FIRECHARGE_USE,
+                target.position(),
+                (0.4F + (0.2F * fireLevel)),
+                (random.triangle(0.8F - (0.1F * fireLevel), 0.1F))
+        );
+        float height = target.getDimensions(target.getPose()).height();
+        spawnFireParticles(target.level(), target.position(), fireLevel, height);
     }
 
     private static void applySharpnessEffect(Entity target, int sharpnessLevel) {
@@ -133,6 +154,27 @@ public class EffectHandler {
 
     private static void spawnBeamImpactParticle(Level level, Vec3 pos, float scale) {
         level.addParticle(new BeamImpactParticleEffect(scale), true, true, pos.x, pos.y + 0.01, pos.z, 0, 0, 0);
+    }
+
+    private static void spawnFireParticles(Level level, Vec3 pos, int count, float height) {
+        final double STEP = height / (count + 1);
+        for (int i=1; i < count+1; ++i) {
+            spawnFireRingParticles(level, pos.add(0, i * STEP, 0));
+        }
+    }
+
+    private static void spawnFireRingParticles(Level level, Vec3 pos) {
+        final int COUNT = 8;
+        final double VEL = 0.1;
+        final double STEP = 2 * Math.PI / COUNT;
+
+        for (int i=0; i < COUNT; ++i) {
+            double radians = STEP * i;
+            double xVel = Math.sin(radians) * VEL;
+            double zVel = Math.cos(radians) * VEL;
+            double yVel = random.triangle(0, 0.01);
+            level.addParticle(ParticleTypes.FLAME, true, true, pos.x, pos.y, pos.z, xVel, yVel, zVel);
+        }
     }
 
     private static void spawnSlashParticle(Level level, Vec3 pos, float scale) {
