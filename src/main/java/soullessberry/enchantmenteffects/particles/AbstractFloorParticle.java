b062c58b26ef1b5e405ca604adc3d0582a1a2d27
soullessberry <skullkidbryan@gmail.com>
2026-08-16 17:38:47 -0700

    Create AbstractFloorParticle

---
+++ src/main/java/soullessberry/enchantmenteffects/particles/AbstractFloorParticle.java
@@ -0,0 +1,45 @@
+package soullessberry.enchantmenteffects.particles;
+
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.renderer.state.level.QuadParticleRenderState;
+import org.joml.Quaternionf;
+import org.jspecify.annotations.NonNull;
+
+public class AbstractFloorParticle extends AbstractScalableParticle {
+
+    private static final float NINETY_DEGREES = (float) Math.toRadians(-90f);
+    private boolean mirrored = false;
+
+    public <T extends ScalableParticleOptions> AbstractFloorParticle(
+            ClientLevel level,
+            double x, double y, double z,
+            double xa, double ya, double za,
+            T params,
+            SpriteSet sprites
+    ) {
+        super(level, x, y, z, xa, ya, za, params, sprites);
+    }
+
+    @Override
+    public void extract(final @NonNull QuadParticleRenderState particleTypeRenderState, final @NonNull Camera camera, final float partialTickTime) {
+        Quaternionf rotation = new Quaternionf();
+        rotation.rotationX(NINETY_DEGREES);
+        this.extractRotatedQuad(particleTypeRenderState, camera, rotation, partialTickTime);
+        rotation.rotationX(NINETY_DEGREES * 3);
+        mirrored = true;
+        this.extractRotatedQuad(particleTypeRenderState, camera, rotation, partialTickTime);
+        mirrored = false;
+    }
+
+    @Override
+    protected float getV0() {
+        return mirrored ? super.getV1() : super.getV0();
+    }
+
+    @Override
+    protected float getV1() {
+        return mirrored ? super.getV0() : super.getV1();
+    }
+}
--- src/main/java/soullessberry/enchantmenteffects/particles/BeamImpactParticle.java
+++ src/main/java/soullessberry/enchantmenteffects/particles/BeamImpactParticle.java
@@ -2,19 +2,14 @@ package soullessberry.enchantmenteffects.particles;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.SpriteSet;
-import net.minecraft.client.renderer.state.level.QuadParticleRenderState;
 import net.minecraft.util.RandomSource;
-import org.joml.Quaternionf;
 import org.jspecify.annotations.NonNull;
 
-public class BeamImpactParticle extends AbstractScalableParticle {
-
-    private static final float NINETY_DEGREES = (float) Math.toRadians(90f);
+public class BeamImpactParticle extends AbstractFloorParticle {
 
     public <T extends ScalableParticleOptions> BeamImpactParticle(
             ClientLevel level,
@@ -25,15 +20,6 @@ public class BeamImpactParticle extends AbstractScalableParticle {
     ) {
         super(level, x, y, z, xa, ya, za, params, sprites);
         this.lifetime = 7;
-    }
-
-    @Override
-    public void extract(final @NonNull QuadParticleRenderState particleTypeRenderState, final @NonNull Camera camera, final float partialTickTime) {
-        Quaternionf rotation = new Quaternionf();
-        rotation.rotationX(NINETY_DEGREES);
-        this.extractRotatedQuad(particleTypeRenderState, camera, rotation, partialTickTime);
-        rotation.rotationX(NINETY_DEGREES * 3);
-        this.extractRotatedQuad(particleTypeRenderState, camera, rotation, partialTickTime);
     }
 
     @Environment(EnvType.CLIENT)
