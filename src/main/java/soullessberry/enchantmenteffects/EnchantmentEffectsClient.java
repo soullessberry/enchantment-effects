c27dabfea3dee34b356e1d7deac376c4941419a5
soullessberry <skullkidbryan@gmail.com>
2026-08-16 17:41:02 -0700

    Create method for client particle registration

--- src/main/java/soullessberry/enchantmenteffects/EnchantmentEffectsClient.java
+++ src/main/java/soullessberry/enchantmenteffects/EnchantmentEffectsClient.java
@@ -2,14 +2,23 @@ package soullessberry.enchantmenteffects;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleProviderRegistry;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
 import soullessberry.enchantmenteffects.particles.*;
 
 public class EnchantmentEffectsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ParticleProviderRegistry.getInstance().register(EnchantmentEffects.BANE_PARTICLE, BaneParticle.Provider::new);
-        ParticleProviderRegistry.getInstance().register(EnchantmentEffects.BEAM_PARTICLE, BeamParticle.Provider::new);
-        ParticleProviderRegistry.getInstance().register(EnchantmentEffects.BEAM_IMPACT_PARTICLE, BeamImpactParticle.Provider::new);
-        ParticleProviderRegistry.getInstance().register(EnchantmentEffects.SLASH_PARTICLE, SlashParticle.Provider::new);
+        registerParticle(EnchantmentEffects.BANE_PARTICLE, BaneParticle.Provider::new);
+        registerParticle(EnchantmentEffects.BEAM_PARTICLE, BeamParticle.Provider::new);
+        registerParticle(EnchantmentEffects.BEAM_IMPACT_PARTICLE, BeamImpactParticle.Provider::new);
+        registerParticle(EnchantmentEffects.SLASH_PARTICLE, SlashParticle.Provider::new);
+    }
+
+    private static <T extends ParticleOptions> void registerParticle(
+            ParticleType<T> type,
+            ParticleProviderRegistry.PendingParticleProvider<T> provider
+    ) {
+        ParticleProviderRegistry.getInstance().register(type, provider);
     }
 }
